0572789c58feaab956546e23ae81baf4f2466a6b
2305079670 <2305079670@qq.com>
2022-10-05 09:52:45 +0800

    2.5

--- ConsoleApplication1/ConsoleApplication1.c
+++ ConsoleApplication1/ConsoleApplication1.c
@@ -2,24 +2,16 @@
 //
 
 #include <stdio.h>
-
+#include <math.h>
 void main()
 {
-    int a, b;
-    float d, e;
-    char c1, c2;
-    long f, g;
-    double m, n;
-    unsigned int x, y;
-
-    a = 65; b = 97;
-    c1 = 'a'; c2 = 'b';
-    d = 3.145; e = -6.874;
-    f = 10000; g = -20000;
-    m = 3141.592627; n = 0.123456789;
-    x = 9527; y = 40000;
-    printf("a=%c,b=%c\n c1=%d,c2=%c \n d=%6.2f,e=%6.2f\n ", a, b, c1, c2, d, e);
-    printf("f=%ld,g=%ld\n m=%15.6f,n=%15.12f \n x=%u,y=%u\n ", f, g, m, n, x, y);
+	double a, b, c, disc, x1, x2, p, q;
+	scanf("%lf,%lf,%lf", &a, &b, &c);
+	disc = b * b - 4 * a * c;
+	p = -b / (2.0 * a);
+	q = sqrt(disc) / (2.0 * a);
+	x1 = p + q; x2 = p - q;
+	printf("x1=%5.2f\n x2=%7.2f\n", x1, x2);
 
 
 }
