0c095c8f2c09d1d1c026d059dc507825809a795a
2305079670 <2305079670@qq.com>
2022-10-05 09:27:08 +0800

    1

--- ConsoleApplication1/ConsoleApplication1.c
+++ ConsoleApplication1/ConsoleApplication1.c
@@ -5,9 +5,19 @@
 
 void main()
 {
-    printf("*******************************************\n");
-    printf("V e r y  g o o d !\n");
-    printf("*******************************************\n");
+    int a, b, c, max;
+    printf("请输入 a,b,c:");
+    scanf("%d %d %d", &a, &b, &c);
+    max = a;
+    if (b > max)
+    {
+        max = b;
+    }
+    if (c > max)
+    {
+        max = c;
+    }
+    printf("最大的数：%d\n", max);
 
     
 }
