66c1eb862cfbde580e2ea2ed2106520309915b16
2305079670 <2305079670@qq.com>
2022-10-05 10:03:25 +0800

    习题2.2

--- ConsoleApplication1/ConsoleApplication1.c
+++ ConsoleApplication1/ConsoleApplication1.c
@@ -5,12 +5,13 @@
 #include <math.h>
 void main()
 {
-	char a, b, c;
-	a = 66; b = 'O'; c = 'Y';
-	putchar(a);
-	putchar(b);
-	putchar(c);
-	putchar('\n');
+	int a, b;
+	float x, y;
+	char c1, c2;
+	scanf("a=%d b=%d", &a, &b);
+	scanf("%f%e", &x, &y);
+	scanf("%c%c", &c1, &c2);
+	printf("a=%d,b=%d,x=%f,y=%f,c1=%c,c2=%c\n", a, b, x, y, c1, c2);
 
 
 }
