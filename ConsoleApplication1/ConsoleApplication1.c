8dc83c6ba45632b1b53e8d834d21854cf27f6b1e
2305079670 <2305079670@qq.com>
2022-10-05 09:29:15 +0800

    2.1

--- ConsoleApplication1/ConsoleApplication1.c
+++ ConsoleApplication1/ConsoleApplication1.c
@@ -2,22 +2,14 @@
 //
 
 #include <stdio.h>
-
+#define price 32
 void main()
 {
-    int a, b, c, max;
-    printf("请输入 a,b,c:");
-    scanf("%d %d %d", &a, &b, &c);
-    max = a;
-    if (b > max)
-    {
-        max = b;
-    }
-    if (c > max)
-    {
-        max = c;
-    }
-    printf("最大的数：%d\n", max);
+    int num, total;
+    num = 10;
+    total = num * price;
+    printf("total = %d\n", total);
+
 
     
 }
