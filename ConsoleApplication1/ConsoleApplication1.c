e7f509ac8391876c7dbcebfb8df80ecfc8060a8b
23050 <23050@ADMIN>
2022-10-05 09:17:40 +0800

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.c
@@ -0,0 +1,16 @@
+// ConsoleApplication1.c : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <stdio.h>
+
+void main()
+{
+    printf("*******************************************\n");
+    printf("V e r y  g o o d !\n");
+    printf("*******************************************\n");
+
+    
+}
+
+
+
