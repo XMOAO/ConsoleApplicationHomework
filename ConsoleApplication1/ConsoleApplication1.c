39561c43f384d893637e3264e394a547974c9c36
2305079670 <2305079670@qq.com>
2022-10-05 09:56:48 +0800

    2.7

--- ConsoleApplication1/ConsoleApplication1.c
+++ ConsoleApplication1/ConsoleApplication1.c
@@ -5,13 +5,12 @@
 #include <math.h>
 void main()
 {
-	double a, b, c, disc, x1, x2, p, q;
-	scanf("%lf,%lf,%lf", &a, &b, &c);
-	disc = b * b - 4 * a * c;
-	p = -b / (2.0 * a);
-	q = sqrt(disc) / (2.0 * a);
-	x1 = p + q; x2 = p - q;
-	printf("x1=%5.2f\n x2=%7.2f\n", x1, x2);
+	char a, b, c;
+	a = 66; b = 'O'; c = 'Y';
+	putchar(a);
+	putchar(b);
+	putchar(c);
+	putchar('\n');
 
 
 }
